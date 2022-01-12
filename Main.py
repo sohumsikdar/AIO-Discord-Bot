e1697146d20531a5d8f4d0a18de12f29f4eb1050
Sohum Sikdar <89215237+sohumsikdar@users.noreply.github.com>
2022-01-12 18:45:07 +0000

    Initial Testing

--- Main.py
+++ Main.py
@@ -1,2 +1,21 @@
 import discord
-print("Hello")
+import os
+
+client = discord.Client();
+
+
+@client.event
+async def on_ready():
+    print('{0.user} logged in'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+tok = os.environ['TOKEN']
+client.run(tok)
